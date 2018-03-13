4d20b6693a9d5e2779487ce91a2cc627f045225f
Unknown <james.smith@heig-vd.ch>
2018-03-13 11:39:52 +0100

    debug size needles

--- src/mcr_labo01_observeurobserve/ClockMechanic.java
+++ src/mcr_labo01_observeurobserve/ClockMechanic.java
@@ -21,9 +21,9 @@ public class ClockMechanic extends Clock{
     final double SECONDE_NEEDLE_THICKNESS = 2;
     final double MINUTE_NEEDLE_THICKNESS = 3;
     final double HOUR_NEEDLE_THICKNESS = 5;
-    final double SECONDE_NEEDLE_SIZE = ((double)PREFERED_SIZE/2) * 0.7;
-    final double MINUTE_NEEDLE_SIZE = ((double)PREFERED_SIZE/2) * 0.5;
-    final double HOUR_NEEDLE_SIZE = ((double)PREFERED_SIZE/2) *0.3;
+    final double SECONDE_NEEDLE_SIZE_RATIO = 0.4;
+    final double MINUTE_NEEDLE_SIZE_RATIO = 0.3;
+    final double HOUR_NEEDLE_SIZE_RATIO = 0.2;
     
     public ClockMechanic(Time t, String img){
         super(t);
@@ -35,15 +35,14 @@ public class ClockMechanic extends Clock{
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        System.out.println(this.getWidth() + " " + this.getHeight());
         g.drawImage(clock, 0, 0,this.getWidth(), this.getHeight(), null);
         drawNeedles(g);
     }
     
     private void drawNeedles(Graphics g){
-        drawNeedle(g, t.getSeconds(), SECONDE_NEEDLE_THICKNESS, SECONDE_NEEDLE_SIZE, Color.RED);
-        drawNeedle(g, t.getMinutes(), MINUTE_NEEDLE_THICKNESS, MINUTE_NEEDLE_SIZE, Color.BLUE);
-        drawNeedle(g, t.getHours() * 5, HOUR_NEEDLE_THICKNESS, HOUR_NEEDLE_SIZE, Color.BLACK);
+        drawNeedle(g, t.getSeconds(), SECONDE_NEEDLE_THICKNESS, SECONDE_NEEDLE_SIZE_RATIO, Color.RED);
+        drawNeedle(g, t.getMinutes(), MINUTE_NEEDLE_THICKNESS, MINUTE_NEEDLE_SIZE_RATIO, Color.BLUE);
+        drawNeedle(g, t.getHours() * 5, HOUR_NEEDLE_THICKNESS, HOUR_NEEDLE_SIZE_RATIO, Color.BLACK);
     }
     
     /** time give in 60 quarter (for hours: hours * 6) **/
@@ -52,8 +51,8 @@ public class ClockMechanic extends Clock{
         double startX = this.getWidth()/2;
         double startY = this.getHeight()/ 2;
         double angle = Math.toRadians(time * 6);
-        double endX = startX + (size * Math.sin(angle));
-        double endY = startY - (size * Math.cos(angle));
+        double endX = startX + ((this.getWidth() *size) * Math.sin(angle));
+        double endY = startY - ((this.getHeight() * size) * Math.cos(angle));
         
         Line2D lin = new Line2D.Double(startX, startY, endX, endY);
         g2.setColor(color);
